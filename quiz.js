78c2957074d081bf0f8e23272afc17e6b4e2efc0
Rafay <abdulrafay34500@gmail.com>
2026-02-13 08:25:48 +0500

    .

---
+++ quiz.js
@@ -0,0 +1,7 @@
+let startButton = document.getElementById('js-start-quiz')
+let body =document.querySelector.body
+
+startButton.addEventListener('click' ,()=>{
+    console.log('j')
+    body.classList.add('.removePage')
+})
