061a865f30ec8a2361ac53a81b2d9633180fc51c
Rafay <abdulrafay34500@gmail.com>
2026-02-13 11:07:47 +0500

    shifting pages with js

--- data/questions.js
+++ data/questions.js
@@ -5,34 +5,34 @@ export const Questions =[
         Option2 : 'Lahore',
         Option3 : 'Rawalpindi',
         Option4 : 'Islamabad',
-        ans : 'Islamabad'
+        
     }, {
         question : 'What is 1 + 1',
         Option1 : '3',
         Option2 : '4',
         Option3 : '2',
-        Option4 : '1',
-        ans : '2'
+        Option4 : '1'
+        
     }, {
         question : 'How many provences are there in pkaistan?',
         Option1 : '1',
         Option2 : '3',
         Option3 : '2',
         Option4 : '4',
-        ans : '4'
+        
     }, {
         question: 'What is javascript?',
         Option1 : 'Language',
         Option2 : 'App',
         Option3 : 'Website',
         Option4 : 'Programing language',
-        ans : 'Programing language'
+       
     },{
         question: 'What is 2+3?',
         Option1 : '6',
         Option2 : '4',
         Option3 : '2',
         Option4 : '5',
-        ans : '5'
+       
     }
 ]
--- quiz.js
+++ quiz.js
@@ -1,6 +1,8 @@
 import {Questions} from './data/questions.js'
 
-console.log(Questions)
+console.log(Questions.length)
+
+
 
 let startButton = document.getElementById('js-start-quiz')
 let parentDiv =document.querySelector('.parent-div')
@@ -11,3 +13,52 @@ startButton.addEventListener('click' ,()=>{
 })
 
 ////////////////////////////////////////////////////
+
+let index=0;
+
+generatingHtml()
+
+function generatingHtml (){
+
+    if (index >=Questions.length){
+        showingResultPage()
+        index =0
+        
+    }
+ 
+    
+    let question =Questions[index]
+    
+    
+
+let Html=`<p class="Question-counter"><span>0</span> Out of 5</p>
+            <h2>${question.question}</h2>
+            
+              <div class="ans-options">
+                <P class="answers js-answers">${question.Option1}</P>
+                <P class="answers js-answers">${question.Option2}</P>
+                <P class="answers js-answers">${question.Option3}</P>
+                <P class="answers js-answers">${question.Option4}</P>
+               </div>`
+
+    document.querySelector('.js-questining-page')
+    .innerHTML =Html;
+
+    document.querySelectorAll('.js-answers').forEach((btn) => {
+
+    btn.addEventListener('click' , ()=>{
+        index++
+        generatingHtml()
+        
+    })
+    });
+
+}
+////////////////////////////////////////////////////////
+
+function showingResultPage() {
+    parentDiv.classList.add('test-Done')
+}
+
+
+
